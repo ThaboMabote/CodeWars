e5bfd609620d8a0e7eabe438f8fc7aa884fa3c3c
ThaboMabote <thabomabote89@gmail.com>
2023-04-10 15:02:28 +0200

    Number To String Kata

---
+++ CodeTest/KataTests/NumberToStringTestClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using CodeWars.Kata;
+namespace CodeTest.KataTests
+{
+	public class NumberToStringTestClass
+	{
+		public NumberToStringTestClass()
+		{
+		}
+
+		//Arrange
+		public static IEnumerable<object[]> TestData => new List<object[]>
+		{
+			new object[] {1, "1"},
+            new object[] {-45, "-45"},
+            new object[] {171, "171"}
+        };
+
+		[Theory]
+		[MemberData(nameof(TestData))]
+		public void ConvertNumberToStringTest(int Input, string expectedValue)
+		{
+			//Act
+			var Response = NumberToString.ConvertNumberToString(Input);
+
+			//Assert
+			Assert.Equal(expectedValue, Response);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void ConvertNumberToStringBadTest(int Input, string expectedValue)
+        {
+            //Act
+            var Response = NumberToString.stringConvertNumberToStringBad(Input);
+
+            //Assert
+            Assert.Equal(expectedValue, Response);
+        }
+    }
+}
